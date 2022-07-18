215f78228aa31472c2d4eb1d83eb7acc56acf00d
Renan Souza <renantrsouza@gmail.com>
2022-07-18 14:08:25 -0400

    aula 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
-
-//Esboço get
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-      }
-    }
-  }
-`
-
-interface Lesson {
-  //temporário
-  id: string
-  title: string
-}
+import { Event } from './pages/Event'
 
 const App = () => {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-  console.log(data)
   return (
     <>
-      <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
-      <ul>
-        {data?.lessons.map((lesson, key) => {
-          return (
-            <li key={key} className="text-zinc-100">
-              {lesson.title}
-            </li>
-          )
-        })}
-      </ul>
+      <Event />
     </>
   )
 }
